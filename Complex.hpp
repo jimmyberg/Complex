8cbeef9d43278ea9821b77e0799a967e06dc531d
Jimmy van den Berg <vandenberg.jimmy@gmaill.com>
2015-10-01 19:07:29 +0200

    added all base functionallity like adding and multiplying

--- Complex.hpp
+++ Complex.hpp
@@ -7,21 +7,149 @@
 
 class Complex{
 public:
+	Complex();
+	Complex(float initR);
 	Complex(float initR, float initI);
 	float real, imaginary;
 	
 	//operators
+	template <typename T> Complex operator+(T right) const;
+	template <typename T> Complex operator-(T right) const;
+	template <typename T> Complex operator*(T right) const;
+	template <typename T> Complex operator/(T right) const;
+
 	Complex operator+(const Complex &right) const;
 	Complex operator-(const Complex &right) const;
 	Complex operator*(const Complex &right) const;
 	Complex operator/(const Complex &right) const;
 
-	Complex& operator+=(const Complex &right);
-	Complex& operator-=(const Complex &right);
-	Complex& operator*=(const Complex &right);
-	Complex& operator/=(const Complex &right);
+	template <typename T> Complex& operator+=(T right);
+	template <typename T> Complex& operator-=(T right);
+	template <typename T> Complex& operator*=(T right);
+	template <typename T> Complex& operator/=(T right);
+
+	Complex& operator+=(const Complex& right);
+	Complex& operator-=(const Complex& right);
+	Complex& operator*=(const Complex& right);
+	Complex& operator/=(const Complex& right);
+
+	//Functuions
+	float absolute();
+	float argument();
 };
 
 std::ostream& operator<<(std::ostream& os, const Complex& complex);
+
+
+Complex::Complex(): real(0), imaginary(0){}
+Complex::Complex(float initR): real(initR), imaginary(0){}
+Complex::Complex(float initR, float initI): real(initR), imaginary(initI){}
+
+template <typename T>
+Complex Complex::operator+(T right) const{
+	return Complex(real + right, imaginary);
+}
+template <typename T>
+Complex Complex::operator-(T right) const{
+	return Complex(real - right, imaginary);
+}
+template <typename T>
+Complex Complex::operator*(T right) const{
+	return Complex(real * right, imaginary * right);
+}
+template <typename T>
+Complex Complex::operator/(T right) const{
+	return Complex(real / right, imaginary / right);
+}
+
+Complex Complex::operator+(const Complex &right) const{
+	return Complex(
+		real + right.real,
+		imaginary + right.imaginary
+		);
+}
+Complex Complex::operator-(const Complex &right) const{
+	return Complex(
+		real - right.real,
+		imaginary - right.imaginary
+		);
+}
+Complex Complex::operator*(const Complex &right) const{
+	return Complex(
+		(real * right.real) - (imaginary * right.imaginary), 
+		(real * right.imaginary) + (imaginary * right.real)
+		);
+}
+Complex Complex::operator/(const Complex &right) const{
+	//Calculate it once.
+	float divider = (right.real * right.real) + (right.imaginary * right.imaginary); 
+	return Complex(
+		((real * right.real) + (imaginary * right.imaginary)) / divider,
+		((imaginary * right.real) - (real * right.imaginary)) / divider
+		);
+}
+
+template <typename T>
+Complex& Complex::operator+=(T right){
+	real += right.real;
+	return *this;
+}
+template <typename T>
+Complex& Complex::operator-=(T right){
+	real -= right.real;
+	return *this;
+}
+template <typename T>
+Complex& Complex::operator*=(T right){
+	real *= right;
+	imaginary *= right;
+	return *this;
+}
+template <typename T>
+Complex& Complex::operator/=(T right){
+	real /= right;
+	imaginary /= right;
+	return *this;
+}
+
+Complex& Complex::operator+=(const Complex& right){
+	real += right.real;
+	imaginary += right.imaginary;
+	return *this;
+}
+Complex& Complex::operator-=(const Complex& right){
+	real -= right.real;
+	imaginary -= right.imaginary;
+	return *this;
+}
+Complex& Complex::operator*=(const Complex& right){
+	float realConserved = real;
+	real = (real * right.real) - (imaginary * right.imaginary);
+	imaginary = (realConserved * right.imaginary) + (right.real * imaginary);
+	return *this;
+}
+Complex& Complex::operator/=(const Complex& right){
+	float realConserved = real;
+	float divider = (right.real * right.real) + (right.imaginary * right.imaginary); 
+	real 		= ((real * right.real) + (imaginary * right.imaginary)) / divider;
+	imaginary 	= ((imaginary * right.real) - (realConserved * right.imaginary)) / divider;
+	return *this;
+}
+
+float Complex::absolute(){
+	return sqrt((real*real) + (imaginary*imaginary));
+}
+float Complex::argument(){
+	return std::atan2(imaginary, real);
+}
+
+std::ostream& operator<<(std::ostream& os, const Complex& complex){
+    os << complex.real;
+    if(complex.imaginary >= 0)
+    	std::cout << '+';
+    std::cout << complex.imaginary << 'i';
+    return os;
+}
+
 
 #endif
--- README.md
+++ README.md
@@ -1,2 +1,39 @@
-# Complex
-A c++ library for complex calculations
+Made by Jimmy van den Berg in The Netherlands
+1 October 2015
+
+c++ library for complex number calculations.
+
+## Example
+
+**main.cpp**
+```c++
+#include <iostream>
+#include "Complex.hpp"
+using namespace std;
+
+int main(int argc, char** argv){
+	Complex a(3, 4);
+	Complex b(1, -2);
+	cout << a << " + " << b << " = " << a + b << endl;
+	cout << a << " - " << b << " = " << a - b << endl;
+	cout << a << " * " << b << " = " << a * b << endl;
+	cout << a << " / " << b << " = " << a / b << endl;
+	cout << a << " * 1.25 = " << a * 1.25 << endl;
+	cout << "Argument of " << b << " = " << b.argument() << endl;
+
+	std::cout << "Press enter to exit." << std::endl;
+	std::cin.get();
+	return 1;
+}
+```
+**output**
+```
+3+4i + 1-2i = 4+2i
+3+4i - 1-2i = 2+6i
+3+4i * 1-2i = 11-2i
+3+4i / 1-2i = -1+2i
+3+4i * 1.25 = 3.75+5i
+Argument of 1-2i = -1.10715
+Press enter to exit.
+```
+
